059c24bd96ad82780cb70d77db19b48b9137a0de
jjcoutts <135303027+jjcoutts@users.noreply.github.com>
2025-05-05 14:21:01 -0400

    Add files via upload

---
+++ Lectures/r_lecture_withinsubjects.R
@@ -0,0 +1,23 @@
+# R script created by Jacob J. Coutts
+# Within-Subjects Designs Analysis
+# PSYC300
+
+# load required packages
+library(ggplot2)
+library(jtools)
+
+# read in data
+drugs <- read.table("drug_data.csv", sep=",",header=TRUE)
+
+# scatterplot of injuries and exhaustion
+gf_boxplot(BUY~NAME, data = drugs, xlab = "Drug Name", ylab = "Willingness to Purchase") + theme_apa()
+
+# wrong t-test
+t.test(BUY~NAME, data = drugs, var.equal=TRUE)
+
+# proper model
+t.test(BUY~NAME, data = drugs, paired=TRUE)
+
+
+
+### end of script
